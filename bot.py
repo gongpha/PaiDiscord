02d8645380b588d4e8d62261e83bca43c3d46d71
Kongfa Warorot <gongpha@hotmail.com>
2018-07-01 22:24:12 +0700

    Add fea

--- bot.py
+++ bot.py
@@ -13,15 +13,21 @@ async def greet(ctx):
 	await ctx.send("WORKS!")
 @bot.command()
 async def add(ctx, a: int, b: int):
-	await ctx.send(">> " + a+b)
+	await ctx.send(">> " + str(a+b))
 @bot.command()
 async def sub(ctx, a: int, b: int):
-	await ctx.send(">> " + a-b)
+	await ctx.send(">> " + str(a-b))
 @bot.command()
 async def mul(ctx, a: int, b: int):
-	await ctx.send(">> " + a*b)
+	await ctx.send(">> " + str(a*b))
 @bot.command()
 async def div(ctx, a: int, b: int):
-	await ctx.send(">> " + a/b)
+	await ctx.send(">> " + str(a/b))
+@bot.command()
+async def sqrt(ctx, a: int):
+	await ctx.send(">> " + str(sqrt(a)))
+@bot.command()
+async def mod(ctx, a: int, b: int):
+	await ctx.send(">> " + str(a%b))
 	
 bot.run('NDU3OTA4NzA3ODE3NDIyODYw.Dhp6wg.tgMLnRYz-43-1Z5x5X-AKHKPXUs')
