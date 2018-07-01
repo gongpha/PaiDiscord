43adeed46ea348ee6a5612d2379e1401171fa6b8
Kongfa Warorot <gongpha@hotmail.com>
2018-07-01 22:16:51 +0700

    Add Fea

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix='?<<')
+bot = commands.Bot(command_prefix='<<?')
 @bot.event
 async def on_ready():
 	print('>> login as')
@@ -11,5 +11,17 @@ async def on_ready():
 @bot.command()
 async def greet(ctx):
 	await ctx.send("WORKS!")
+@bot.command()
+async def add(ctx, a: int, b: int):
+	await ctx.send(">> " + a+b)
+@bot.command()
+async def sub(ctx, a: int, b: int):
+	await ctx.send(">> " + a-b)
+@bot.command()
+async def mul(ctx, a: int, b: int):
+	await ctx.send(">> " + a*b)
+@bot.command()
+async def div(ctx, a: int, b: int):
+	await ctx.send(">> " + a/b)
 	
 bot.run('NDU3OTA4NzA3ODE3NDIyODYw.Dhp6wg.tgMLnRYz-43-1Z5x5X-AKHKPXUs')
