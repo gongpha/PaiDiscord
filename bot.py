213314f19ea1b1c8c5c624c84d4724426b2d8db3
Kongfa Warorot <gongpha@hotmail.com>
2019-01-02 21:43:45 +0700

    Add some commands

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import math
 import asyncio
 import datetime
 import ncfu
+import statistics
 from discord.ext import commands
 
 import random
@@ -50,9 +51,30 @@ token = os.environ.get('BOT_TOKEN',None)
 openprocch = int(os.environ.get('OPENPROC_CH',None))
 
 
-bot = commands.Bot(command_prefix='<<?')
+bot = commands.Bot(command_prefix='::')
 client = discord.Client()
 # bot.remove_command("help")
+
+def errtype(etype) :
+	if etype == 0 : 
+		return "Null Error (IDK Too. What is this XD)"
+	elif etype == 1 :
+		return "Arguments error"
+	else :
+		return "Wtf is this ?"
+		
+def embed_error(ctxx, strr : str, vall : str, etype : int) :
+	errembed=discord.Embed(title="❌ Oops! There's something error", description=errtype(etype), color=0xff0000)
+	errembed.add_field(name=strr,value=vall)
+	return errembed
+	
+def idToUser(bot, ctx, idthat)
+	if not str.isdigit(idthat) :
+			user = await bot.get_user_info(ctx.message.mentions[0].id)
+		else :
+			user = await bot.get_user_info(idthat)
+	return user
+
 
 async def status_task():
 	print("Starting Task...")
@@ -102,43 +124,103 @@ async def on_ready():
 	print(bot.user.name)
 	print(bot.user.id)
 	print('>> Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-	
-@bot.command()
-async def say(ctx):
-	"""Just say 'I'm still here.'"""
-	await ctx.send("I'm still here.")
-@bot.command()
-async def ncfunamegenmons(ctx):
-	"""Use NCFUDemo to generate MONS name (UNSTABLE)"""
-	await ctx.send(">> `"+ ncfuDemo_genMonsGeneral() + "`")
-@bot.command()
-async def ncfunt(ctx, a: str):
-	"""Use NCFU to generate string using own template"""
-	await ctx.send(">> `"+ ncfu.n_t_generate(a) + "`")
-@bot.command()
-async def add(ctx, a: int, b: int):
-	"""Add 2 Numbers"""
-	await ctx.send(">> `" + str(a+b) + "`")
-@bot.command()
-async def sub(ctx, a: int, b: int):
-	"""Subtract 2 Numbers"""
-	await ctx.send(">> `" + str(a-b) + "`")
-@bot.command()
-async def mul(ctx, a: int, b: int):
-	"""Multiply 2 Numbers"""
-	await ctx.send(">> `" + str(a*b) + "`")
-@bot.command()
-async def div(ctx, a: int, b: int):
-	"""Divide 2 Numbers"""
-	await ctx.send(">> `" + str(a/b) + "`")
-@bot.command()
-async def sqrt(ctx, a: int):
-	"""SquareRoot of Number"""
-	await ctx.send(">> `" + str(math.sqrt(a)) + "`")
-@bot.command()
-async def mod(ctx, a: int, b: int):
-	"""Modulo 2 Numbers"""
-	await ctx.send(">> `" + str(a%b) + "`")
 
+class TestCommand:
+	@bot.command()
+	async def error_embed(ctx, method : str, solution : str, errtype : int) :
+		"""Testing Embed with Error"""
+		await ctx.send("Here, <@" + str(ctx.message.author.id) + ">", embed=embed_error(ctx, method, solution, errtype))
+		
+class BasicCommand :
+	@bot.command()
+	async def say(ctx):
+		"""Just say 'I'm still here.'"""
+		await ctx.send("I'm still here.")
+	@bot.command()
+	async def ncfunamegenmons(ctx):
+		"""Use NCFUDemo to generate MONS name (UNSTABLE)"""
+		await ctx.send(">> `"+ ncfuDemo_genMonsGeneral() + "`")
+	@bot.command()
+	async def ncfunt(ctx, a: str):
+		"""Use NCFU to generate string using own template"""
+		await ctx.send(">> `"+ ncfu.n_t_generate(a) + "`")
+	@bot.command()
+	async def add(ctx, a: int, b: int):
+		"""Add 2 Numbers"""
+		await ctx.send(">> `" + str(a+b) + "`")
+	@bot.command()
+	async def sub(ctx, a: int, b: int):
+		"""Subtract 2 Numbers"""
+		await ctx.send(">> `" + str(a-b) + "`")
+	@bot.command()
+	async def mul(ctx, a: int, b: int):
+		"""Multiply 2 Numbers"""
+		await ctx.send(">> `" + str(a*b) + "`")
+	@bot.command()
+	async def div(ctx, a: int, b: int):
+		"""Divide 2 Numbers"""
+		await ctx.send(">> `" + str(a/b) + "`")
+	@bot.command()
+	async def sqrt(ctx, a: int):
+		"""SquareRoot of Number"""
+		await ctx.send(">> `" + str(math.sqrt(a)) + "`")
+	@bot.command()
+	async def mod(ctx, a: int, b: int):
+		"""Modulo 2 Numbers"""
+		await ctx.send(">> `" + str(a%b) + "`")
+	@bot.command()
+	async def msginfo(ctx):
+		"""This Message Information"""
+		await ctx.send("Author : `" + str(ctx.message.author) + 
+		"`\nChannel ID : `" + str(ctx.message.channel) + "`\nContent : `" + str(ctx.message.content) + "`")
+	@bot.command()
+	async def mentionme(ctx):
+		"""Ping myself"""
+		await ctx.send("<@" + str(ctx.message.author.id) + ">")
+	@bot.command()
+	async def mention(ctx, idthat = none):
+		"""Ping Him!"""
+		user = idToUser(bot, ctx, idthat)
+		if not idthat:
+			await ctx.send("Uh oh, <@" + str(ctx.message.author.id) + ">", embed = embed_error(ctx, "You missed argument!", "Put an argument then try again", 1))
+		else : 
+			if not user  :
+				await ctx.send("Uh oh, <@" + str(ctx.message.author.id) + ">", embed = embed_error(ctx, "User wasn't found in the channel!", "Make sure, you put a correct User ID or that user has been in the channel", 1))
+			else :
+				await ctx.send("<@" + str(user.id) + ">")
+
+	@bot.command()
+	async def myavatar(ctx):
+		"""Your Avatar URL"""
+		await ctx.send("`" + str(ctx.message.author) + "` : " + str(ctx.message.author.avatar_url))
+	@bot.command()
+	async def avatar(ctx, idthat = none):
+		"""His Avatar URL"""
+		user = idToUser(bot, ctx, idthat)
+		if not idthat:
+			await ctx.send("Uh oh, <@" + str(ctx.message.author.id) + ">", embed = embed_error(ctx, "You missed argument!", "Put an argument then try again", 1))
+		else : 
+			if not user  :
+				await ctx.send("Uh oh, <@" + str(ctx.message.author.id) + ">", embed = embed_error(ctx, "User wasn't found!", "Make sure, you put a correct User ID", 1))
+			else :
+				await ctx.send("`" + str(user) + "` : " + str(user.avatar_url))
+
+class StatsCommand :
+	@bot.command()
+	async def mean(ctx, *a):
+		"""Finding Arithmetic mean"""
+		await ctx.send(">> `" + str(statistics.mean(list(map(int, a)))) + "`")
+	@bot.command()
+	async def h_mean(ctx, *a):
+		"""Finding Harmonic mean"""
+		await ctx.send(">> `" + str(statistics.harmonic_mean(list(map(int, a)))) + "`")
+	@bot.command()
+	async def median(ctx, *a):
+		"""Finding Median (Middle)"""
+		await ctx.send(">> `" + str(statistics.median(list(map(int, a)))) + "`")
+
+#bot.add_cog(TestCommand())
+#bot.add_cog(BasicCommand())
+#bot.add_cog(StatsCommand())
 bot.loop.create_task(status_task())
 bot.run(token)
