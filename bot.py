adee79c3e6f1c0b7937439fed4a54247014a6636
Kongfa Waroros <gongpha@hotmail.com>
2019-04-24 21:46:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import yaml
 with open('configs/proc.yml') as f:
     proclist = yaml.safe_load(f)
 
-for p in proclist["proc_dev" if len(sys.argv) <= 1 else sys.argv[1] + ".py"] :
+for p in proclist["proc_dev" if len(sys.argv) <= 1 else sys.argv[1]] :
     with open('configs/{}.yml'.format(p), encoding="utf8") as f:
         config = yaml.safe_load(f)
 
